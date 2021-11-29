0221817182426c84e436b4d29a7ba83cedf5abf3
Li Haoyi <haoyi.sg@gmail.com>
2021-11-29 21:43:36 +0800

    Fix hygiene of macros #12
    Fixes https://github.com/com-lihaoyi/Ammonite/issues/953
    
    Review by @lolgab @jodersky

---
+++ mainargs/test/src/HygieneTests.scala
@@ -0,0 +1,26 @@
+package mainargs
+import utest._
+
+
+object HygieneTests extends TestSuite{
+
+  object Main{
+    @main
+    def run(@arg(short = 'f', doc = "String to print repeatedly")
+            foo: String,
+            @arg(name = "my-num", doc = "How many times to print string")
+            myNum: Int = 2,
+            @arg(doc = "Example flag")
+            bool: Flag) = {
+      foo * myNum + " " + bool.value
+    }
+  }
+
+  val tests = Tests {
+    import scala.collection.mutable._
+    test("importingSeqShouldntFailCompile"){
+      ParserForMethods(Main)
+    }
+
+  }
+}
