923c24616e333da23997951556e0ebd45a02d56c
Li Haoyi <haoyi.sg@gmail.com>
2024-01-25 12:28:08 +0800

    .

--- mainargs/src/TokenGrouping.scala
+++ mainargs/src/TokenGrouping.scala
@@ -26,6 +26,16 @@ object TokenGrouping {
       .flatMap { x => getNames(x).map(_ -> x) }
       .toMap[String, ArgSig]
 
+    lazy val shortArgMap: Map[Char, ArgSig] = argSigs
+      .collect{case (a, _) => a.shortName.map(_ -> a)}
+      .flatten
+      .toMap[Char, ArgSig]
+
+    lazy val shortFlagArgMap: Map[Char, ArgSig] = argSigs
+      .collect{case (a, r: TokensReader.Flag) => a.shortName.map(_ -> a)}
+      .flatten
+      .toMap[Char, ArgSig]
+
     lazy val keywordArgMap = makeKeywordArgMap(
       x => x.name.map("--" + _) ++ x.shortName.map("-" + _)
     )
@@ -43,7 +53,55 @@ object TokenGrouping {
             m.get(k).map(a => (a, a.reader))
           }
 
-          if (head.startsWith("-") && head.exists(_ != '-')) {
+
+          // special handling for combined short args of the style "-xvf" or "-j10"
+          if (head.startsWith("-") &&
+            head.lift(1).exists(c => c != '-' && c != '=') &&
+            head.lift(2).exists(c => c != '-' && c != '=')){
+            val chars = head.drop(1)
+            var rest2 = rest
+            var i = 0
+            var currentMap = current
+            var failure: Result.Failure = null
+
+            while (i < chars.length){
+              val c = chars(i)
+              shortFlagArgMap.get(c) match{
+                case Some(a) =>
+                  // For `Flag`s in chars, we consume the char, set it to `true`, and continue
+                  currentMap = Util.appendMap(currentMap, a, "")
+                  i += 1
+                case None =>
+                  // For other kinds of short arguments, we consume the char, set the value to
+                  // the remaining characters, and exit
+                  shortArgMap.get(c) match{
+                    case Some(a) =>
+                      if (i == chars.length - 1) {
+                        rest2 match{
+                          case Nil =>
+                            failure = Result.Failure.MismatchedArguments(incomplete = Some(a))
+                          case next :: remaining =>
+                            currentMap = Util.appendMap(currentMap, a, next)
+                            rest2 = remaining
+                        }
+                      }else {
+                        currentMap = Util.appendMap(currentMap, a, chars.drop(i + 1))
+                      }
+                    case None =>
+                      println("C")
+                      // If we encounter a character that is neither a short flag or a
+                      // short argument, it is an error
+                      failure = Result.Failure.MismatchedArguments(unknown = Seq(c.toString))
+                  }
+                  i = chars.length
+              }
+
+            }
+
+            if (failure != null) failure
+            else rec(rest2, currentMap)
+
+          } else if (head.startsWith("-") && head.exists(_ != '-')) {
             head.split("=", 2) match{
               case Array(first, second) =>
                 lookupArgMap(first, longKeywordArgMap) match {
--- mainargs/test/src/FlagTests.scala
+++ mainargs/test/src/FlagTests.scala
@@ -5,34 +5,119 @@ object FlagTests extends TestSuite {
 
   object Base {
     @main
-    def flaggy(a: Flag, b: Boolean, c: Flag) = a.value || b || c.value
+    def flaggy(a: Flag, b: Boolean, c: Flag) = Seq(a.value, b, c.value)
   }
   val check = new Checker(ParserForMethods(Base), allowPositional = true)
 
   val tests = Tests {
     test - check(
       List("-b", "true"),
-      Result.Success(true)
+      Result.Success(Seq(false, true, false))
     )
     test - check(
       List("-b", "false"),
-      Result.Success(false)
+      Result.Success(Seq(false, false, false))
     )
 
     test - check(
       List("-a", "-b", "false"),
-      Result.Success(true)
+      Result.Success(Seq(true, false, false))
     )
 
     test - check(
       List("-c", "-b", "false"),
-      Result.Success(true)
+      Result.Success(Seq(false, false, true))
     )
 
     test - check(
       List("-a", "-c", "-b", "false"),
-      Result.Success(true)
+      Result.Success(Seq(true, false, true))
     )
+
+    test("combined"){
+      test - check(
+        List("-bfalse"),
+        Result.Success(List(false, false, false))
+      )
+      test - check(
+        List("-btrue"),
+        Result.Success(List(false, true, false))
+      )
+
+      test - check(
+        List("-abtrue"),
+        Result.Success(List(true, true, false))
+      )
+      test - check(
+        List("-abfalse"),
+        Result.Success(List(true, false, false))
+      )
+
+      test - check(
+        List("-a", "-btrue"),
+        Result.Success(List(true, true, false))
+      )
+
+      test - check(
+        List("-a", "-bfalse"),
+        Result.Success(List(true, false, false))
+      )
+
+      test - check(
+        List("-acbtrue"),
+        Result.Success(List(true, true, true))
+      )
+
+      test - check(
+        List("-acbfalse"),
+        Result.Success(List(true, false, true))
+      )
+
+      test - check(
+        List("-a", "-c", "-btrue"),
+        Result.Success(List(true, true, true))
+      )
+
+      test - check(
+        List("-a", "-c", "-bfalse"),
+        Result.Success(List(true, false, true))
+      )
+
+      test - check(
+        List("-a", "-btrue", "-c"),
+        Result.Success(List(true, true, true))
+      )
+
+      test - check(
+        List("-a", "-bfalse", "-c"),
+        Result.Success(List(true, false, true))
+      )
+
+      test - check(
+        List("-ba"),
+        Result.Failure.InvalidArguments(
+          List(
+            Result.ParamError.Failed(
+              new ArgSig(None, Some('b'), None, None, mainargs.TokensReader.BooleanRead, false, false),
+              Vector("a"),
+              "java.lang.IllegalArgumentException: For input string: \"a\""
+            )
+          )
+        )
+      )
+
+      test - check(
+        List("-ab"),
+        Result.Failure.MismatchedArguments(
+          Nil,
+          Nil,
+          Nil,
+          Some(
+            new ArgSig(None, Some('b'), None, None, TokensReader.BooleanRead, false, false)
+          )
+        )
+      )
+    }
 
   }
 }
